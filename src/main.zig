f2116d73633b27d292b52ac730a61968d2c5fcd2
Mandip Gurung <grngd00@gmail.com>
2023-04-23 17:54:28 +0900

    update for latest zig http client implementation

--- src/main.zig
+++ src/main.zig
@@ -5,12 +5,13 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer {
         const leaked = gpa.deinit();
-        if (leaked) {
+        if (leaked == .leak) {
             @panic("leaked");
         }
     }
     const allocator = gpa.allocator();
     var runtime = Runtime.new(allocator, &handler);
+    defer runtime.deinit();
     try runtime.start();
 }
 
@@ -48,7 +49,7 @@ const Runtime = struct {
     }
 
     fn start(self: *Self) !void {
-        const api_endpoint = std.os.getenv("AWS_LAMBDA_RUNTIME_API") orelse "";
+        const api_endpoint = std.os.getenv("AWS_LAMBDA_RUNTIME_API") orelse "grng.dev";
         try self.setEndpoint(api_endpoint);
 
         while (true) {
@@ -61,15 +62,16 @@ const Runtime = struct {
 
     fn next(self: *Self) !Context {
         const endpoint = try std.Uri.parse(self.endpoint_next);
-        var req = try self.http_client.request(endpoint, .{
-            .user_agent = user_agent,
-        }, .{});
+        var client_headers = std.http.Headers{ .allocator = self.allocator };
+        defer client_headers.deinit();
+        var req = try self.http_client.request(.GET, endpoint, client_headers, .{});
         defer req.deinit();
+        try req.start();
         try req.do();
-        var ch = CustomHeader.init(req.response.parser.header_bytes.items);
+        var id = req.response.headers.getFirstValue("Lambda-Runtime-Aws-Request-Id") orelse "requestidnotfound";
         var body = try req.reader().readAllAlloc(self.allocator, 1024 * 1024 * 10);
         return .{
-            .request_id = try ch.get("Lambda-Runtime-Aws-Request-Id"),
+            .request_id = id,
             .body = body,
         };
     }
@@ -78,14 +80,15 @@ const Runtime = struct {
         const respondUrl = try std.fmt.allocPrint(self.allocator, "{s}{s}/response", .{ self.endpoint_next, ctx.request_id });
         defer self.allocator.free(respondUrl);
         var uri = try std.Uri.parse(respondUrl);
-        var req = try self.http_client.request(uri, .{
-            .user_agent = user_agent,
-            .method = .POST,
-        }, .{});
+        var client_headers = std.http.Headers{ .allocator = self.allocator };
+        defer client_headers.deinit();
+        var req = try self.http_client.request(.POST, uri, client_headers, .{});
         defer req.deinit();
-        // try req.writer().writeAll(body);
+        req.transfer_encoding = .{ .content_length = 14 };
+        try req.start();
+        try req.writeAll(body);
+        try req.finish();
         try req.do();
-        _ = try req.write(body);
     }
 
     fn setEndpoint(self: *Self, endoint: []const u8) !void {
@@ -103,45 +106,3 @@ const Runtime = struct {
 };
 
 const user_agent = "lambda-zig";
-
-test "testclient" {
-    const expect = std.testing.expect;
-    _ = expect;
-    var http_client = std.http.Client{ .allocator = std.testing.allocator };
-    defer http_client.deinit();
-    const uri = try std.Uri.parse("https://grng.dev");
-    var req = try http_client.request(uri, .{}, .{});
-    defer req.deinit();
-    try req.do();
-    std.debug.print("\n{s}", .{req.response.parser.header_bytes.items});
-    var ch = CustomHeader.init(req.response.parser.header_bytes.items);
-    const v = try ch.get("Access-Control-Allow-Origin");
-    std.debug.print("\n{s}\n", .{v});
-}
-
-const CustomHeader = struct {
-    const Self = @This();
-    raw_data: []u8,
-
-    fn init(raw_header: []u8) @This() {
-        return .{ .raw_data = raw_header };
-    }
-
-    pub const Headererrors = error{InvalidHttpHeader};
-
-    fn get(self: *Self, key: []const u8) Headererrors![]const u8 {
-        var itr = std.mem.tokenize(u8, self.raw_data, "\r\n");
-        _ = itr.next() orelse return error.InvalidHttpHeader;
-        while (itr.next()) |line| {
-            if (line.len == 0) {
-                continue;
-            }
-            var kvitr = std.mem.tokenize(u8, line, ": ");
-            const k = kvitr.next() orelse return Headererrors.InvalidHttpHeader;
-            if (std.ascii.eqlIgnoreCase(k, key)) {
-                return kvitr.rest();
-            }
-        }
-        return "";
-    }
-};
